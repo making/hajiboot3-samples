5374e45127a8f532db919006b8d8bdc893c8990e
Toshiaki Maki <tmaki@vmware.com>
2022-12-23 13:23:11 +0900

    add hajiboot3-markdown-printer-without-di

---
+++ 05/hajiboot3-markdown-printer-without-di/src/main/java/hajiboot/Hajiboot3MarkdownPrinterApplication.java
@@ -0,0 +1,13 @@
+package hajiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hajiboot3MarkdownPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hajiboot3MarkdownPrinterApplication.class, args);
+	}
+
+}
